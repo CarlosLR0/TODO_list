eac05066c36242988c8afb574c7816d8d43572e5
Carlos Lopez <rlcarlosvladimir@gmail.com>
2022-10-17 13:10:46 -0400

    Edited comments in insert_TODO.py

--- insert_TODO.py
+++ insert_TODO.py
@@ -1,17 +1,15 @@
-# Ask user for their name
+# Ask user for their task to add
 TODO_list = input("Add to the list: ")
 
 # After done the asking, the program opens the file
-# 'usernames.txt' in write mode, there are some more other
-# modes, but in this example, I will only use the write mode with
+# 'TODO_list.txt' in write mode
 with open("TODO_list.txt", "a") as fl:
     # Now, 'fl' is the reference of the command 'open("TODO_list.txt", "w")'
-    # The file will write the username that it got
+    # The file will write the task that it got
     # at the first part
 	fl.write('\n')
 	fl.write(TODO_list)
     
-    # Now, the program done the adding, you should close them
+    # Now, the programs done the adding, and should close them
 	fl.close()
 
-# That's it!
