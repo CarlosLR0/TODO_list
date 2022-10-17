af998fc5f328b288896a9fc7e63716f040994062
Carlos Lopez <rlcarlosvladimir@gmail.com>
2022-10-17 12:39:55 -0400

    Changes to TODO_list.txt and insert_TODO.py

--- insert_TODO.py
+++ insert_TODO.py
@@ -1,14 +1,10 @@
 # Ask user for their name
-TODO_list = []
-list_length = 100
-for i in range(list_length):
-	todo = input("Add to the list: ")
-	TODO_list.append(todo)
+TODO_list = input("Add to the list: ")
 
 # After done the asking, the program opens the file
 # 'usernames.txt' in write mode, there are some more other
 # modes, but in this example, I will only use the write mode with
-with open("TODO_list.txt", "w") as fl:
+with open("TODO_list.txt", "a") as fl:
     # Now, 'fl' is the reference of the command 'open("TODO_list.txt", "w")'
     # The file will write the username that it got
     # at the first part
