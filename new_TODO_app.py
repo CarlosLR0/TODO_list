8209a7828223af356cfe8652a87ab23abcb6ed7f
Carlos <rlcarlosvladimir@gmail.com>
2022-10-17 21:58:06 -0400

    adding newest todo application

---
+++ new_TODO_app.py
@@ -0,0 +1,34 @@
+print("\nMy TODO_list")
+instructions = "\nOptions: \n-Enter a to add new task. \n-Enter c to mark item as completed. \n-Enter d to delete items. \n-Enter v to view list. \n-Enter e to exit "
+print(instructions)
+
+todo_list = list()
+
+i = True
+
+while i:
+    user_in = input("Enter your option: ").lower()
+
+    if user_in == 'a':
+        task = input("Type the new task: ").lower()
+        todo_list.append(task)
+        print("Added new task: ", task)
+
+    elif user_in == 'c':
+        #for idx, task in enumerate(todo_list)
+        int_list_item = int(input("Which index do you want to mark as completed? "))
+        #modify
+        c = ("completed")
+        todo_list[int_list_item] = todo_list[int_list_item], c
+        print(todo_list[int_list_item])
+        
+    elif user_in == 'v':
+        print("Current list")
+        print(todo_list)
+            
+    elif user_in == 'e':
+        exit_in = input("\nDo you want to exit? (Y/N): ").lower()
+        if exit_in == 'y':
+            print("Good Bye")
+            i = False
+        
