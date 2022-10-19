8ce63ec2f72f04dbcab3aa38f661534d5755cd05
Carlos <rlcarlosvladimir@gmail.com>
2022-10-18 21:23:15 -0400

    Added load option and edited old options

--- new_TODO_app.py
+++ new_TODO_app.py
@@ -1,5 +1,5 @@
 print("\nMy TODO_list")
-instructions = "\nOptions: \n-Enter a to add new task. \n-Enter c to mark item as completed. \n-Enter d to delete items. \n-Enter v to view list. \n-Enter e to exit "
+instructions = "\nOptions: \n-Enter a to add new task. \n-Enter c to mark item as completed. \n-Enter d to delete items. \n-Enter e to exit. \n-Enter l to load saved list. \n-Enter s to save current list. \n-Enter v to view list. "
 print(instructions)
 
 todo_list = []
@@ -7,45 +7,70 @@ todo_list = []
 i = True
 
 while i:
-    user_in = input("Enter your option: ").lower()
+    user_in = input("\nEnter your option: ").lower()
 
     if user_in == 'a':
-        task = input("Type the new task: ").lower()
-        todo_list.append([task])
-        print("Added new task: ", task)
+        task = input("\nType the new task: ")
+        todo_list.append(task)
+        print("\nAdded new task: ", task)
 
     elif user_in == 'c':
-        #for idx, task in enumerate(todo_list)
-        int_list_item = int(input("Which index do you want to mark as completed? "))
-        #modify
-        c = ("completed")
-        todo_list[int_list_item] = todo_list[int_list_item], c
-        print(todo_list[int_list_item])
+       try:
+           int_list_item = int(input("\nWhich index do you want to mark as completed? "))
+           c = ("completed")
+           todo_list[int_list_item] = todo_list[int_list_item], c
+           print(todo_list[int_list_item])
 
+       except IndexError:
+            print("\nIndex is out of range") 
+       except ValueError:
+           print("\nEnter an integer")
+            
     elif user_in == 'd':
-        print("Current list")
-        for task in range(len(todo_list)):
-            print(task,end = ". ")
-            print(todo_list[task])
-        del_task_index = int(input("Which index do you want to delete? "))
-        print("You have deleted",todo_list.pop(del_task_index))
-        
+        print("\nCurrent list")
+        try:
+            for task in range(len(todo_list)):
+                print(task,end = ". ")
+                print(todo_list[task])
+            del_task_index = int(input("Which index do you want to delete? "))
+            print("\nYou have deleted",todo_list.pop(del_task_index))
+
+        except IndexError:
+            print("\nIndex is out of range")
+        except ValueError:
+            print("\nEnter an integer")
+            
     elif user_in == 'v':
-        print("Current list")
+        print("\nCurrent list")
         for task in range(len(todo_list)):
            print(task,end =". ")
            print(todo_list[task])
             
     elif user_in == 'e':
-        exit_in = input("\nDo you want to exit? (Y/N): ").lower()
+        exit_in = input("\nUnsaved tasks will not be recoverable\nDo you want to exit? (y/n): ")
         if exit_in == 'y':
             print("Good Bye")
-            i = False
+            quit()
 
     elif user_in == 's':
-        save_in = input("Are you sure you want to save? (Y/N: ")
+        save_in = input("\nAre you sure you want to save? (y/n): ")
         if save_in == 'y':
             with open("TODO_list.txt", newline='\n', mode='w') as fl:
                 fl.write('\n'.join(str(e) for e in todo_list))
                 fl.close()
                 print("Current list has been saved")
+
+    elif user_in == 'l':
+        read = input("\n\nWARNING\nLoading a saved list will overwrite your current list\nPress enter after reading the warning")
+        load_in = input('Do you want to load your saved list? (y/n): ')
+        if load_in == 'y':
+            
+            a = open("TODO_list.txt", mode='r')
+            b = a.readlines()
+            todo_list = [x.replace('\n','') for x in b]
+            print("Loaded saved list:")
+            for task in range(len(todo_list)):
+                print(task, end =". ")
+                print(todo_list[task])
+            
+
